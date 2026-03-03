96826e47b2718d6a74f11a37bde39aa055eb71f9
Faaz666 <fs09911@st.habib.edu.pk>
2026-03-03 12:02:51 +0500

    Lab7

--- Lab07/Lab07_Verilog/Lab07_Verilog.srcs/sources_1/new/top_alu.v
+++ Lab07/Lab07_Verilog/Lab07_Verilog.srcs/sources_1/new/top_alu.v
@@ -6,38 +6,38 @@ module top_alu(
 
 wire rst = switches[15];
 
+wire WriteEnable;
+wire [4:0] rs1, rs2, rd;
+wire [31:0] WriteData;
+wire [3:0] ALUControl;
 wire [4:0] state;
-wire init_done;
-wire WE_init;
-wire [4:0] rd_init;
-wire [31:0] WD_init;
-
-RF_ALU_FSM fsm(
-    .clk(clk),
-    .rst(rst),
-    .state(state),
-    .init_done(init_done),
-    .WriteEnable_init(WE_init),
-    .rd_init(rd_init),
-    .WriteData_init(WD_init)
-);
 
 wire [31:0] ReadData1, ReadData2;
 wire [31:0] ALUResult;
 wire Zero;
 
-wire WE = init_done ? switches[13] : WE_init;
-wire [4:0] rd = init_done ? switches[12:8] : rd_init;
-wire [31:0] WD = init_done ? ALUResult : WD_init;
+RF_ALU_FSM fsm(
+    .clk(clk),
+    .rst(rst),
+    .ALUResult(ALUResult),
+    .Zero(Zero),
+    .WriteEnable(WriteEnable),
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(rd),
+    .WriteData(WriteData),
+    .ALUControl(ALUControl),
+    .state(state)
+);
 
 regfile RF(
     .clk(clk),
     .rst(rst),
-    .WriteEnable(WE),
-    .rs1(5'd1),
-    .rs2(5'd2),
+    .WriteEnable(WriteEnable),
+    .rs1(rs1),
+    .rs2(rs2),
     .rd(rd),
-    .WriteData(WD),
+    .WriteData(WriteData),
     .ReadData1(ReadData1),
     .readData2(ReadData2)
 );
@@ -45,16 +45,16 @@ regfile RF(
 ALU alu(
     .A(ReadData1),
     .B(ReadData2),
-    .ALUControl(switches[3:0]),
+    .ALUControl(ALUControl),
     .ALUResult(ALUResult),
     .Zero(Zero)
 );
 
-assign leds_out[3:0] = ALUResult[3:0];
-assign leds_out[7:4] = state[3:0];
+assign leds_out[3:0]  = ALUResult[3:0];
+assign leds_out[7:4]  = state[3:0];
 assign leds_out[12:8] = rd;
-assign leds_out[13] = Zero;
-assign leds_out[14] = WE;
-assign leds_out[15] = rst;
+assign leds_out[13]   = Zero;
+assign leds_out[14]   = WriteEnable;
+assign leds_out[15]   = rst;
 
 endmodule
